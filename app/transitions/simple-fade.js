a94b6816c7798679ecb0885290887f66675a4d56
Edward Faulkner <ef@alum.mit.edu>
2015-03-04 09:26:46 -0800

    formatting

--- app/transitions/simple-fade.js
+++ app/transitions/simple-fade.js
@@ -2,7 +2,15 @@ import Ember from "ember";
 var Velocity = Ember.$.Velocity;
 
 export default function fade() {
-  return Velocity.animate(this.oldElement, {opacity: 0}, { duration: 500 }).then(() => {
-    return Velocity.animate(this.newElement, {opacity: [ 1, 0] }, { duration: 500, visibility: 'visible' });
+  return Velocity.animate(
+    this.oldElement,
+    { opacity: 0 },
+    { duration: 500 }
+  ).then(() => {
+    return Velocity.animate(
+      this.newElement,
+      {opacity: [ 1, 0] },
+      { duration: 500, visibility: 'visible' }
+    );
   });
 }
