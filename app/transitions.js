836b393735230be9c70d5fea7754ce0781302a96
Edward Faulkner <ef@alum.mit.edu>
2015-03-04 10:58:13 -0800

    slow down

--- app/transitions.js
+++ app/transitions.js
@@ -1,12 +1,15 @@
 export default function() {
+
+  var duration = 500;
+
   this.transition(
     this.fromRoute('emberconf.speakers'),
     this.toRoute('emberconf.speaker'),
     this.use('explode', {
       pickNew: 'img',
-      use: 'toUp'
+      use: ['toUp', { duration } ]
     }, {
-      use: 'toLeft'
+      use: ['toLeft', { duration } ]
     }),
     this.reverse('toRight')
   );
